2cf8cef313f2ae4fcf005b3749c87781d8e509cd
rickpatel1999 <37589090+rickpatel1999@users.noreply.github.com>
2018-03-20 16:07:07 +0000

    Update Coursework.java

--- Coursework.java
+++ Coursework.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class coursework implements ActionListener
+public class Coursework implements ActionListener
 {
 	JFrame a = new JFrame();
 	JPanel panel = new JPanel();
